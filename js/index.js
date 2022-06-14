40b2ddcc75798b700504304c79d01f53a03c75d7
BaekJinH <88886246+BaekJinH@users.noreply.github.com>
2022-06-15 00:56:50 +0900

    slide 수정 버튼 움직이게 하기

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ $(function(){
 })
 $(function(){
     $('.open').click(function(){
-        document.getElementById('sub_Menu').style.height='500px';
+        document.getElementById('sub_Menu').style.height='300px';
     })
     $('.close').click(function(){
         document.getElementById('sub_Menu').style.height='0%'
@@ -17,3 +17,25 @@ $(function(){
 function close(){
     document.getElementById('sub_Menu').style.height='0%'
 }
+
+function slide(){
+    $('.mySlide').stop().animate({marginLeft:'-30vw'},1000,function(){
+        $('.mySlide li:first').appendTo('.mySlide');
+        $('.mySlide').css({marginLeft:0})
+    })
+}
+setInterval(slide,5000)
+
+
+
+
+$(function(){
+    $('input').focus(function(){
+        document.getElementsByClassName('popup')[0].style.height='300px';
+        $('.popup').css({boxShadow:'0 0 50px'})
+    })
+    $('input').blur(function(){
+        $('.popup').height(0);
+    })
+
+})
