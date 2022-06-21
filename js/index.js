33ff609568232625b8fd302ee73bd2b8b7f7075c
BaekJinH <tmdfyd6868@naver>
2022-06-21 18:09:22 +0900

    인트로 글자 수정 / 내용 추가 / 애니메이션 추가

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ $(function(){
 $(function(){
     $('.open').click(function(){
         // $('#sub_Menu').height(280)
-        $('#sub_Menu').height(500)
+        $('#sub_Menu').height(350)
     })
     $('.close').click(function(){
         // $('#sub_Menu').animate({height:0})
@@ -21,7 +21,7 @@ $(function(){
 
 $(function(){
     $('.semi_Menu').hide();
-        $('.flex_Box > li').mouseover(function(){
+    $('.flex_Box > li').mouseover(function(){
         $(this).find('.semi_Menu').stop().slideDown();
     })
     $('.flex_Box > li').mouseout(function(){
@@ -33,8 +33,9 @@ $(function(){
 
 // 아래 인트로 슬라이드
 
+
 $(function(){
-    $('.intro_wrap button').click(function(){
+    $('.intro_wrap button').not('.behind').click(function(){
         $('.intro_wrap').stop().animate({marginLeft:'-100vw'},800,function(){
             $('.intro_wrap li:first').appendTo('.intro_wrap');
             $('.intro_wrap').css({marginLeft:0})
@@ -44,25 +45,35 @@ $(function(){
 
 
 
+$(function(){
+    $('.behind').click(function(){
+        // $('.text4').css({transform:'translateY(-100%)'})
+        // $('.text4').stop().animate({top:'-100%'})
+    })
+})
+
+
+
+
 
 // 인트로 4번 div 호버시 커밍 순 글자 제거
 
 // 아래는 글자 작업하려고 앞으로 당겨놓음
 
-// $(function(){
-//     $('.intro_wrap').animate({marginLeft:'-300vw'})
-// })
-
-// 
 $(function(){
-    $('.intro_Image4').not('.text4').on({click:function(){
-        // $(this).addClass('up')
-        // 제이쿼리로 클릭시 상자 내리기
-        $('.text4').stop().animate({top:'-50%'},500);
-    },mouseout:function(){
-        $('.text4').stop().animate({top:'50%'},500)
-    }})
+    $('.intro_wrap').animate({marginLeft:'-300vw'})
 })
+
+// 보류
+// $(function(){
+//     $('.intro_Image4').not('.text4').on({click:function(){
+//         // $(this).addClass('up')
+//         // 제이쿼리로 클릭시 상자 내리기
+//         $('.text4').stop().animate({top:'-50%'},500);
+//     },mouseout:function(){
+//         $('.text4').stop().animate({top:'50%'},500)
+//     }})
+// })
 
 
 // 시간지연 후 ㅅ원하는 애니메이션 끝나고 이미지 커지게
@@ -179,6 +190,18 @@ $(function(){
 //     })
 // })
 
+// 인트로 버튼 누르면 글자 fade효과
+// $(function(){
+//     var i = 0;
+//     var btn = $('.text > button').length
+//     $('.text > button').click(function(){
+//         i++
+//         $(this).index(i);
+//         $('.text').animate({marginRight:'50px'})
+//     })
+// })
+
+
 
 // 아래 메인 멀티플 슬라이드
 
@@ -292,8 +315,19 @@ $(function(){
 })
 
 
+// 섹션 버튼 클릭시 이미지 회전 / 보류
 
+// $(function(){
+//     $('.main_txt').find('button').click(function(){
+//         $(this).parent().css({transform:'rotateY(180deg)'})
+//     })
+// })
 
+$(function(){
+    $('.btn_Box > button').eq(0).click(function(){
+        $('.container').children().show();
+    })
+})
 
 
 $(function(){
@@ -302,17 +336,34 @@ $(function(){
 
 $(function(){
     $('input').on({'click keydown focus':function(){
-        document.getElementsByClassName('popup')[0].style.height='300px';
-        $('.popup').css({boxShadow:'0 0 50px'});
-        $('.btn_Box').css({display:'block'}).fadeDown('slow').height(300);
+        $('.popup').stop().animate({height:350},100);
+        $('.popup').css({boxShadow:'0 0 8px white'});
+        $('.btn_Box').css({display:'block'}).fadeDown('slow').height(350);
     }})
-
     $('.input_Cont').mouseleave(function(){
         $('.popup').height(0);
         $('input[type=text').val('')
         $('.btn_Box').fadeUp('slow').height(0);
     })
 })
+
+
+// 필터
+$(function(){
+    $('input:text').on({'keyup focus click':function(){
+        var value = $(this).val().toUpperCase();
+        $('.container').children().filter(function(){
+            $(this).toggle($(this).text().toUpperCase().indexOf(value) > -1)
+        })
+    }})
+    $('.btn_Box button').click(function(){
+        var value2 = $(this).val().toUpperCase();
+        $('.container').children().filter(function(){
+            $(this).toggle($(this).text().toUpperCase().indexOf(value2) > -1)
+        })
+    })
+})
+
 
 
 // 검색어 값 가져오기
@@ -326,12 +377,48 @@ $(function(){
     })
 })
 
+// 그림 호버시 글자 생성 / 내려왔다 올라가는 버젼
 
+$(function(){
+    $('section > div').on({mouseover:function(){
+        $(this).find('.main_txt').css({transform:'translateY(0%)',transition:'1s'});
+        // $('.main_txt').stop().animate({},800);
+    },mouseout:function(){
+        $(this).find('.main_txt').css({transform:'translateY(-100%)',transition:'1s'});
+        
+    }})
+})
 
+// 내려왔다가 밑으로 가고 위에서 다시 내려오게 만들기
 
+// $(function(){
+//     $('section > div').on({mouseover:function(){
+//         $(this).find('.main_txt').css({transform:'translateY(0%)',transition:'1s'});
+//     },mouseout:function(){
+//         $(this).find('.main_txt').css({transform:'translateY(100%)',transition:'1s'});
+//     }})
+// })
 
+// $(function(){
+//     function down(){
+//         $('section > div').mouseover(function(){
+//             $(this).find('.main_txt').css({transform:'translateY(0%)',transition:'1s'});
+//         })
+//     }
+//     function up(){
+//         $('section > div').mouseout(function(){
+//             $(this).find('.main_txt').css({transform:'translateY(100%)',transition:'1s'})        
+//         })
+//     }
+//     $('section div').mouseover(function(){
+//         down()
+//     })
+//     $('section div').mouseover(function(){
+//         up()
+//     })
+// })
 
-// 밑 두개 햄버거
+// 밑 두개 햄버거 / 뜯어보기
 
 // var burger = $('.menu-trigger');
 
@@ -349,7 +436,6 @@ $(function(){
 //   if (this.className == 'on') this.classList.remove('on');
 //   else this.classList.add('on');
 // });
-
 
 
 
@@ -371,17 +457,62 @@ $(function(){
 
 
 
+// $(function(){
+//     var hei = $('.semi_Menu li').height();
+//     $('.semi_Menu li').on({'mouseenter focusin':function(){
+//         var index = $(this).index();
+//         $('.semi_Menu span').stop().animate({top:index * hei},'fast')
+//     }});
+//     $('.semi_Menu li:eq(0)').trigger('mouseenter')
+// });
+
+
+
+
+//  footer 스크롤 탑으로
 $(function(){
-    var hei = $('.semi_Menu li').height();
-    $('.semi_Menu').append('<span></span>');
-    $('.semi_Menu li').on({'mouseenter focusin':function(){
-        var index = $(this).index();
-        $('.semi_Menu span').stop().animate({top:index * hei},'fast')
-    }});
-    $('.semi_Menu li:eq(0)').trigger('mouseenter')
-});
+    $('.scrollTop').click(function(){
+        var offset = $('header').offset();
+        $('html').animate({scrollTop:offset.top},1200)
+    })
+})
 
-
+// 서브 메뉴 홈페이지 앵커
+// $(function(){
+//     $('.flex_Box > li').click(function(){
+//         var i  = $('.flex_Box > li').index();
+//         if(i == 0){
+//             var offset = $('.slide_Head').offset();
+//             $('html').animate({scrollTop:offset.top},800);
+//         }
+//         else if(i == 1){
+//             var offset = $('.input_Cont').offset();
+//             $('html').animate({scrollTop:offset.top},800);
+//         }
+//     })
+// })
+$(function(){
+    $('.flex_Box > li').eq(0).click(function(){
+        var offset = $('.slide_Head').offset();
+        $('html').animate({scrollTop:offset.top},800);
+    })
+    $('.flex_Box > li').eq(1).click(function(){
+        var offset = $('.input_Cont').offset();
+        $('html').animate({scrollTop:offset.top},800);
+    })
+    $('.flex_Box > li').eq(2).click(function(){
+        var offset = $('.main2').offset();
+        $('html').animate({scrollTop:offset.top},800);
+    })
+    // $('.flex_Box > li').eq(3).click(function(){
+    //     var offset = $('.slide_Head').offset();
+    //     $('html').animate({scrollTop:offset.top},800);
+    // })
+    // $('.flex_Box > li').eq(4).click(function(){
+    //     var offset = $('.slide_Head').offset();
+    //     $('html').animate({scrollTop:offset.top},800);
+    // })
+})
 
 
 $(function(){
