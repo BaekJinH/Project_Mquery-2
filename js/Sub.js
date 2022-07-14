70a506b617b5b7dbefd97edb97c522ffc9d29113
BaekJinH <88886246+BaekJinH@users.noreply.github.com>
2022-07-15 02:11:03 +0900

    .

--- js/Sub.js
+++ js/Sub.js
@@ -342,106 +342,106 @@ $(function(){
 
 // 밑의 슬라이드를 반복문으로 사용해서 태그별로 슬라이드를 적용해야함
 
-function Slider(target, type) {
-    // 상태
-    let index = 1;
-    let isMoved = true;
-    const speed = 1000; // ms
+// function Slider(target, type) {
+//     // 상태
+//     let index = 1;
+//     let isMoved = true;
+//     const speed = 1000; // ms
   
-    // 방향
-    const transform = "transform " + speed / 5000 + "s";
-    let translate = (i) => "translateX(-" + 100 * i + "%)";
-    if (type === "V") {
-      translate = (i) => "translateY(-" + 100 * i + "%)";
-    }
+//     // 방향
+//     const transform = "transform " + speed / 5000 + "s";
+//     let translate = (i) => "translateX(-" + 100 * i + "%)";
+//     if (type === "V") {
+//       translate = (i) => "translateY(-" + 100 * i + "%)";
+//     }
   
-    // 슬라이더
-    const slider = document.querySelector('.dlcScreen');
-    const sliderRects = slider.getClientRects()[0];
-    slider.style["overflow"] = "hidden";
+//     // 슬라이더
+//     const slider = document.querySelector('.dlcScreen');
+//     const sliderRects = slider.getClientRects()[0];
+//     slider.style["overflow"] = "hidden";
   
-    // 슬라이더 화면 컨테이너
-    const container = document.createElement("div");
-    container.style["display"] = "flex";
-    container.style["flex-direction"] = type === "V" ? "column" : "row";
-    container.style["width"] = sliderRects.width + "px";
-    container.style["height"] = sliderRects.height + "px";
-    container.style["transform"] = translate(index);
+//     // 슬라이더 화면 컨테이너
+//     const container = document.createElement("div");
+//     container.style["display"] = "flex";
+//     container.style["flex-direction"] = type === "V" ? "column" : "row";
+//     container.style["width"] = sliderRects.width + "px";
+//     container.style["height"] = sliderRects.height + "px";
+//     container.style["transform"] = translate(index);
   
-    // 슬라이더 화면 목록
-    let boxes = [].slice.call(slider.children);
-    boxes = [].concat(boxes[boxes.length - 1], boxes, boxes[0]);
+//     // 슬라이더 화면 목록
+//     let boxes = [].slice.call(slider.children);
+//     boxes = [].concat(boxes[boxes.length - 1], boxes, boxes[0]);
   
-    // 슬라이더 화면 스타일
-    const size = boxes.length;
-    for (let i = 0; i < size; i++) {
-      const box = boxes[i];
-      box.style["flex"] = "none";
-      box.style["flex-wrap"] = "wrap";
-      box.style["height"] = "100%";
-      box.style["width"] = "100%";
-      container.appendChild(box.cloneNode(true));
-    }
+//     // 슬라이더 화면 스타일
+//     const size = boxes.length;
+//     for (let i = 0; i < size; i++) {
+//       const box = boxes[i];
+//       box.style["flex"] = "none";
+//       box.style["flex-wrap"] = "wrap";
+//       box.style["height"] = "100%";
+//       box.style["width"] = "100%";
+//       container.appendChild(box.cloneNode(true));
+//     }
   
-    // 처음/마지막 화면 눈속임 이벤트
-    container.addEventListener("transitionstart", function () {
-      isMoved = false;
-      setTimeout(() => {
-        isMoved = true;
-      }, speed);
-    });
-    container.addEventListener("transitionend", function () {
-      // 처음으로 순간이동
-      if (index === size - 1) {
-        index = 1;
-        container.style["transition"] = "none";
-        container.style["transform"] = translate(index);
-      }
-      // 끝으로 순간이동
-      if (index === 0) {
-        index = size - 2;
-        container.style["transition"] = "none";
-        container.style["transform"] = translate(index);
-      }
-    });
+//     // 처음/마지막 화면 눈속임 이벤트
+//     container.addEventListener("transitionstart", function () {
+//       isMoved = false;
+//       setTimeout(() => {
+//         isMoved = true;
+//       }, speed);
+//     });
+//     container.addEventListener("transitionend", function () {
+//       // 처음으로 순간이동
+//       if (index === size - 1) {
+//         index = 1;
+//         container.style["transition"] = "none";
+//         container.style["transform"] = translate(index);
+//       }
+//       // 끝으로 순간이동
+//       if (index === 0) {
+//         index = size - 2;
+//         container.style["transition"] = "none";
+//         container.style["transform"] = translate(index);
+//       }
+//     });
   
-    // 슬라이더 붙이기
-    slider.innerHTML = "";
-    slider.appendChild(container);
+//     // 슬라이더 붙이기
+//     slider.innerHTML = "";
+//     slider.appendChild(container);
   
-    return {
-      move: function (i) {
-        if (isMoved === true) {
-          index = i;
-          container.style["transition"] = transform;
-          container.style["transform"] = translate(index);
-        }
-      },
-      next: function () {
-        if (isMoved === true) {
-          index = (index + 1) % size;
-          container.style["transition"] = transform;
-          container.style["transform"] = translate(index);
-        }
-      },
-      prev: function () {
-        if (isMoved === true) {
-          index = index === 0 ? index + size : index;
-          index = (index - 1) % size;
-          container.style["transition"] = transform;
-          container.style["transform"] = translate(index);
-        }
-      }
-    };
-  }
+//     return {
+//       move: function (i) {
+//         if (isMoved === true) {
+//           index = i;
+//           container.style["transition"] = transform;
+//           container.style["transform"] = translate(index);
+//         }
+//       },
+//       next: function () {
+//         if (isMoved === true) {
+//           index = (index + 1) % size;
+//           container.style["transition"] = transform;
+//           container.style["transform"] = translate(index);
+//         }
+//       },
+//       prev: function () {
+//         if (isMoved === true) {
+//           index = index === 0 ? index + size : index;
+//           index = (index - 1) % size;
+//           container.style["transition"] = transform;
+//           container.style["transform"] = translate(index);
+//         }
+//       }
+//     };
+//   }
   
-  const s1 = new Slider("#slider1", "H");
-  const s2 = new Slider("#slider2", "V");
+//   const s1 = new Slider("#slider1", "H");
+//   const s2 = new Slider("#slider2", "V");
   
-  setInterval(() => {
-    s1.next();
-    s2.next();
-  }, 1000)
+//   setInterval(() => {
+//     s1.next();
+//     s2.next();
+//   }, 1000)
 
 
 
@@ -580,7 +580,7 @@ $(function(){
         }
 
         
-        if(scrollValue > 3400){
+        if(scrollValue > 3200){
             $('.slideIn3').css({right:'100%'})
             $('.slideRight h4').eq(2).fadeIn(3000)
             $('.slideRight p').eq(2).fadeIn(3000)
@@ -593,7 +593,7 @@ $(function(){
         
 
 
-        if(scrollValue > 4100){
+        if(scrollValue > 3800){
             $('.slideIn4').css({right:'0%'})
             $('.slideRight h4').eq(3).fadeIn(3000)
             $('.slideRight p').eq(3).fadeIn(3000)
